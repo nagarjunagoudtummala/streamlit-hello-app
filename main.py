959113ca6b70ead5ffdd21a690c9992fc06cf733
nagarjuna-tummala_tmna <nagarjuna.tummala@toyota.com>
2025-01-31 19:11:19 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+st.write("This is a simple Streamlit app.")
+
+name = st.text_input("Enter your name:")
+if name:
+    st.write(f"Hello, {name}!")
